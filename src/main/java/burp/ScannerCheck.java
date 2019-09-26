bcd3f4de9ba4c83ebb7167c4b81bf2e435a3f804
unknown <alexandre.teyar@gmail.com>
2019-09-26 12:14:06 +0100

    Code improvements
    * Adding more modularity to the code
    * Fixing the travis.yml config file

--- src/main/java/burp/ScannerCheck.java
+++ src/main/java/burp/ScannerCheck.java
@@ -32,55 +32,55 @@ public class ScannerCheck implements IScannerCheck {
   }
 
   // TODO: Ensure that CrimeFlare sends a response back, if blacklisted print an alert
-  private String queryService(IHttpRequestResponse baseRequestResponse) {
+  private boolean queryCrimeFlare(String host) {
     IExtensionHelpers helpers = this.burpExtenderCallbacks.getHelpers();
-    byte[] response = null;
+    byte[] response;
 
     try {
       URL url = new URL("http://www.crimeflare.org:82/cgi-bin/cfsearch.cgi");
       byte[] request = helpers.buildHttpRequest(url);
-      IParameter parameter = helpers
-          .buildParameter("cfS", baseRequestResponse.getHttpService().getHost(),
-              IParameter.PARAM_BODY);
-      request = helpers.addParameter(request, parameter);
+      request = helpers
+          .addParameter(request, helpers.buildParameter("cfS", host, IParameter.PARAM_BODY));
       request = helpers.toggleRequestMethod(request);
 
       // this.burpExtenderCallbacks.printOutput(helpers.bytesToString(request));
-
       response = burpExtenderCallbacks
           .makeHttpRequest("crimeflare.org", 82, false, request);
-
       // this.burpExtenderCallbacks.printOutput(helpers.bytesToString(response));
+
+      if (helpers.bytesToString(response).contains("these are not CloudFlare-user nameservers.")) {
+        this.burpExtenderCallbacks.printOutput("No entry found in the CloudFlare database");
+        return false;
+      } else {
+        this.burpExtenderCallbacks.printOutput("Existing entry found in the CloudFlare database");
+        return true;
+      }
     } catch (MalformedURLException e) {
       this.burpExtenderCallbacks
-          .printError(String.format("Malformed URL exception: %s", e.getMessage()));
+          .printError(String.format("%s", e.getMessage()));
       this.burpExtenderCallbacks
-          .issueAlert(String.format("Malformed URL exception: %s", e.getMessage()));
+          .issueAlert(String.format("%s", e.getMessage()));
     }
 
-    return helpers.bytesToString(response);
+    return false;
   }
 
   @Override
   public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse) {
     List<IScanIssue> scanIssues = new ArrayList<>();
+    String host = baseRequestResponse.getHttpService().getHost();
 
     // TODO: Check that the below code behaves as expected, and that we are not sending
     // TODO: more than 1 request to CrimeFlare per host being scanned
     // Check if the host has already been queried against the CrimeFlare database in order to
     // reduce the total number of requests sent
-    if (!this.hosts.contains(baseRequestResponse.getHttpService().getHost())) {
-      if (queryService(baseRequestResponse)
-          .contains("these are not CloudFlare-user nameservers.")) {
-        assert true;
-        this.burpExtenderCallbacks.printOutput("No entry found in the CloudFlare database");
-      } else {
-        scanIssues.add(new ScanIssue(burpExtenderCallbacks, baseRequestResponse));
-        this.burpExtenderCallbacks.printOutput("Existing entry found in the CloudFlare database");
+    if (!this.hosts.contains(host)) {
+      if (queryCrimeFlare(host)) {
+        scanIssues.add(new ScanIssue(this.burpExtenderCallbacks, baseRequestResponse));
       }
-
-      this.hosts.add(baseRequestResponse.getHttpService().getHost());
     }
+
+    this.hosts.add(baseRequestResponse.getHttpService().getHost());
 
     return scanIssues;
   }
